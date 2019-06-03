621b0280168ccda4bd172159bf641e4d6e5a5100
Dan Schultz <schultzsdaniel@gmail.com>
2019-06-03 11:07:25 -0700

    Updates

---
+++ precache-manifest.22c46c81e2ee702946e6984302a03332.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f17325d9343c13c20b57e1dca4f3d84",
+    "url": "/1920s-murder-mystery-party/index.html"
+  },
+  {
+    "revision": "db1c6cadafbb574591be",
+    "url": "/1920s-murder-mystery-party/static/css/main.e2217dca.chunk.css"
+  },
+  {
+    "revision": "8e233ede8b469e05c116",
+    "url": "/1920s-murder-mystery-party/static/js/2.55ed60b1.chunk.js"
+  },
+  {
+    "revision": "db1c6cadafbb574591be",
+    "url": "/1920s-murder-mystery-party/static/js/main.550a9a7e.chunk.js"
+  },
+  {
+    "revision": "fc4931121035d1d577e6",
+    "url": "/1920s-murder-mystery-party/static/js/runtime~main.f6a3c2fa.js"
+  },
+  {
+    "revision": "732630b09dfe1b9d85a220862464e647",
+    "url": "/1920s-murder-mystery-party/static/media/Core-Deco.732630b0.ttf"
+  },
+  {
+    "revision": "8813b178b7eea848a196592c87be93db",
+    "url": "/1920s-murder-mystery-party/static/media/Hollywood-Sign.8813b178.svg"
+  },
+  {
+    "revision": "f40bf7d03d07822515ef1efb76104bfc",
+    "url": "/1920s-murder-mystery-party/static/media/Light-Shape.f40bf7d0.png"
+  },
+  {
+    "revision": "a4592c0502bdeaf4d150654a14a87bc9",
+    "url": "/1920s-murder-mystery-party/static/media/Male-Closeup.a4592c05.png"
+  },
+  {
+    "revision": "8139def08327a00c5ead87e82bab3e00",
+    "url": "/1920s-murder-mystery-party/static/media/Poster-Girl.8139def0.png"
+  },
+  {
+    "revision": "548233c893abbf46498a77d3d4acae74",
+    "url": "/1920s-murder-mystery-party/static/media/Poster-Rules.548233c8.png"
+  },
+  {
+    "revision": "2daa63258587ec7c4016e10ac2c16804",
+    "url": "/1920s-murder-mystery-party/static/media/Stars.2daa6325.svg"
+  },
+  {
+    "revision": "6c7d455ebdab9db672942d8842d0ed71",
+    "url": "/1920s-murder-mystery-party/static/media/abacus.6c7d455e.png"
+  },
+  {
+    "revision": "5332dcfd7476d7208b6c473ccdae3384",
+    "url": "/1920s-murder-mystery-party/static/media/broken-heart.5332dcfd.png"
+  },
+  {
+    "revision": "078d95051924b040996ba3a76e1c190d",
+    "url": "/1920s-murder-mystery-party/static/media/door-slot.078d9505.png"
+  },
+  {
+    "revision": "9236939a1e65d93cda43f046eeb3a839",
+    "url": "/1920s-murder-mystery-party/static/media/drink.9236939a.png"
+  },
+  {
+    "revision": "12daf545981796c515de27ce7473ea8b",
+    "url": "/1920s-murder-mystery-party/static/media/kind.12daf545.png"
+  },
+  {
+    "revision": "8334d35744c15a3e84f9713319082b85",
+    "url": "/1920s-murder-mystery-party/static/media/lightbulb.8334d357.png"
+  },
+  {
+    "revision": "3752f5cfef50ec9adb4282ebc72c97df",
+    "url": "/1920s-murder-mystery-party/static/media/lips.3752f5cf.png"
+  },
+  {
+    "revision": "541ff316bf6ccddbd5809ffe7f914b31",
+    "url": "/1920s-murder-mystery-party/static/media/medal.541ff316.png"
+  },
+  {
+    "revision": "03743310430c3898c883127acd5f9ae0",
+    "url": "/1920s-murder-mystery-party/static/media/mirror.03743310.png"
+  },
+  {
+    "revision": "f78eee8bc380374b9414a07be07919ca",
+    "url": "/1920s-murder-mystery-party/static/media/note.f78eee8b.png"
+  },
+  {
+    "revision": "fb2f84e15efc3e2a3e5bbe571cc3e48d",
+    "url": "/1920s-murder-mystery-party/static/media/question-mark.fb2f84e1.png"
+  },
+  {
+    "revision": "4464f012a632dc7324032faa507e7055",
+    "url": "/1920s-murder-mystery-party/static/media/spirit.4464f012.png"
+  }
+]);
